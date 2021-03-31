f8fcdd3c06068ca9e60b10c98740da9d800e7409
SebastianVeloza <fernando7829@hotmail.com>
2021-03-31 18:19:49 -0500

    TraerUsuarios

--- src/Componentes/ListarUsers/Users.js
+++ src/Componentes/ListarUsers/Users.js
@@ -5,8 +5,31 @@ async function usersProcess(){
     var user=[];
     await Admin.getUser()
     .then((res)=>{
+        let arr = [];
         console.log(res);
-    })
+        
+    })}
+  /*  .then(async (data)=>{
 
-}
+        
+        for (let i = 0; i < data.length; i++) {
+          
+          let res = await File.getUser(data[i].__id);
+        dato.id
+        dato.username
+        dato.email
+        dato.name
+        dato.type_user
+        dato.maxsize
+        
+          arr.push(user)
+        }
+        return arr
+      }) 
+      .then(arr => user = arr)
+      
+      console.log('user: ',user)
+      return user;
+
+}*/ 
 export default usersProcess;
