3adb087baddf5e5181acaa291a9030f7e66eeab0
SebastianVeloza <fernando7829@hotmail.com>
2021-04-09 11:36:01 -0500

    Arreglo de esquema, y de promesas.

--- src/Componentes/ListarUsers/Listar.js
+++ src/Componentes/ListarUsers/Listar.js
@@ -3,10 +3,10 @@ import React from 'react';
 import '../../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter } from 'reactstrap';
-import {ListarUsers} from './Users';
+import { ListarUsers } from './Users';
 import Admin from '../../Request/admin';
 
-
+//const [users, setUsers] = useState(null);
 //const data = ListarUsers;
 //console.log(data);
 export class TablaUser extends React.Component {
@@ -16,181 +16,189 @@ export class TablaUser extends React.Component {
     this.mostrarModalActualizar = this.mostrarModalActualizar.bind(this);
     this.eliminar = this.eliminar.bind(this);
   }
-    state = {
-        data: [],
-        modalActualizar: false,
-        modalInsertar: false,
-        form: {
-         // _id: "",
-            username: "",
-            email: "",
-            name: "",
-            type_user: "",
-            maxsize: "",
+  state = {
+    data: [],
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      _id: "",
+      username: "",
+      email: "",
+      name: "",
+      type_user: "",
+      maxsize: "",
 
-        },
-    };
+    },
+  };
+
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
+
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
     
-      mostrarModalActualizar = (dato) => {
-        this.setState({
-          form: dato,
-          modalActualizar: true,
-        });
-      };
+    //promesa
+    Admin.EditUser(dato._id, dato.maxsize)
     
-      cerrarModalActualizar = () => {
-        this.setState({ modalActualizar: false });
-      };
-    
-      mostrarModalInsertar = () => {
-        this.setState({
-          modalInsertar: true,
-        });
-      };
-    
-      cerrarModalInsertar = () => {
-        this.setState({ modalInsertar: false });
-      };
-    
-      editar = (dato) => {
-        var contador = 0;
-        var arreglo = this.state.data;
-        //promesa
-        Admin.EditUser(dato.id,dato.maxsize).then(data =>{
+    .then(data => {
+      arreglo.map((registro) => {
+        if (dato._id == registro._id) {
+
+          arreglo[contador].maxsize = dato.maxsize;
+
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+
+    })
+
+  };
+
+  eliminar = (dato) => {
+    var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento " + dato.username);
+    if (opcion == true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      //promesa
+      Admin.DeleteUser(dato._id)
+        .then(data => {
           arreglo.map((registro) => {
-            if (dato.id == registro.id) {
-              
-              arreglo[contador].maxsize = dato.maxsize;
-  
+            if (dato._id == registro._id) {
+              arreglo.splice(contador, 1);
             }
             contador++;
           });
           this.setState({ data: arreglo, modalActualizar: false });
 
         })
-       
-      };
-    
-      eliminar = (dato) => {
-        var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+dato.id);
-        if (opcion == true) {
-          var contador = 0;
-          var arreglo = this.state.data;
-          //promesa
-          Admin.DeleteUser(dato.id)
-          .then(data => {
-            arreglo.map((registro) => {
-              if (dato.id == registro.id) {
-                arreglo.splice(contador, 1);
-              }
-              contador++;
-            });
-            this.setState({ data: arreglo, modalActualizar: false });
 
-          })
-          
-        }
-      };
-    
-      insertar= ()=>{
-        var valorNuevo= {...this.state.form};
-       // valorNuevo.id=this.state.data.length+1;//Nuevo id
-        
-        // Admin.CreateUser("Sebas",
-        // "fernando7829@hotmail.com",
-        // "sebas",
-        // "ADMIN",
-         //5)
-        // promesa
-        Admin.CreateUser(valorNuevo.username,
-          valorNuevo.email,
-          valorNuevo.name,
-          valorNuevo.type_user,
-          parseInt(valorNuevo.maxsize)
-          )
-          .then(data =>
-            {
-              var lista= this.state.data;
-              lista.push(valorNuevo);
-              this.setState({ modalInsertar: false, data: lista });
-            })
-          console.log(valorNuevo.username);
-          console.log(valorNuevo.email);
-          console.log(valorNuevo.name);
-          console.log(valorNuevo.type_user);
-          console.log(valorNuevo.username);
-          
-            
-            window.location.reload();
+    }
+  };
 
-        
-       
-      }
-    
-      handleChange = (e) => {
-        this.setState({
-          form: {
-            ...this.state.form,
-            [e.target.name]: e.target.value,
-          },
-        });
-      };
-      
-      getListUser=async()=>{
-        await ListarUsers.then(
-          data=>this.setState({...this.state,data})
-        )
-      }
-      
+  insertar = () => {
+    var valorNuevo = { ...this.state.form };
+    // valorNuevo.id=this.state.data.length+1;//Nuevo id
 
-      componentDidMount(){
-        this.getListUser()
-      }
+    // Admin.CreateUser("Sebas",
+    // "fernando7829@hotmail.com",
+    // "sebas",
+    // "ADMIN",
+    //5)
+    // promesa
+    Admin.CreateUser(valorNuevo.username,
+      valorNuevo.email,
+      valorNuevo.name,
+      valorNuevo.type_user,
+      parseInt(valorNuevo.maxsize)
+    )
+      .then(data => {
+        var lista = this.state.data;
+        lista.push(valorNuevo);
+        this.setState({ modalInsertar: false, data: lista });
+      })
+    console.log(valorNuevo.username);
+    console.log(valorNuevo.email);
+    console.log(valorNuevo.name);
+    console.log(valorNuevo.type_user);
+    console.log(valorNuevo.username);
+
+
+    window.location.reload();
+
+
+
+  }
+  // syncUser = () => {
+  //   ListarUsers.then(
+  //     array => setUsers(array))
+  // }
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  getListUser = async () => {
+    await ListarUsers.then(
+      data => this.setState({ ...this.state, data }),
       
-      render() {
-        
-        
-      const tabladinamica = this.state.data.map(dato =>
-          <RegistroFila dato = {dato}
-          mostrarModalActualizar = {this.mostrarModalActualizar}
-          eliminar = {this.eliminar}/>);
-       
-        setTimeout(console.log, 2000, tabladinamica ) 
-       /*  console.log(this.state.data,
-          "imprimeintp"
-          ) */
-        return (
-          <>
-            <Container>
-            <br />
-              <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Crear</Button>
-              <br />
-              <br />
-              <Table>
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>Email</th>
-                    <th>Name</th>
-                    <th>Type_user</th>
-                    <th>Maxsize</th>
-                    <th>Acción</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {tabladinamica}
-                </tbody>
-              </Table>
-            </Container>
-    
-            <Modal isOpen={this.state.modalActualizar}>
-              <ModalHeader>
-               <div><h3>Editar Registro</h3></div>
-              </ModalHeader>
-    
-              <ModalBody>
-                {/* <FormGroup>
+    )
+  }
+
+
+  componentDidMount() {
+    this.getListUser()
+  }
+
+  render() {
+
+
+    const tabladinamica = this.state.data.map(dato =>
+      <RegistroFila dato={dato}
+        key={dato._id}
+        mostrarModalActualizar={this.mostrarModalActualizar}
+        eliminar={this.eliminar} />);
+
+    setTimeout(console.log, 2000, tabladinamica)
+    /*  console.log(this.state.data,
+       "imprimeintp"
+       ) */
+    return (
+      <>
+        <Container>
+          <br />
+          <Button color="success" onClick={() => this.mostrarModalInsertar()}>Crear</Button>
+          <br />
+          <br />
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Name</th>
+                <th>Type_user</th>
+                <th>Maxsize</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tabladinamica}
+            </tbody>
+          </Table>
+        </Container>
+
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+            <div><h3>Editar Registro</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+             <FormGroup>
                   <label>
                    Id:
                   </label>
@@ -199,23 +207,23 @@ export class TablaUser extends React.Component {
                     className="form-control"
                     readOnly
                     type="text"
-                    value={this.state.form.id}
+                    value={this.state.form._id}
                   />
-                </FormGroup> */}
-                
-                 <FormGroup>
-                  <label>
-                    Username: 
+                </FormGroup> 
+
+            {/* <FormGroup>
+              <label>
+                Username:
                   </label>
-                  <input
-                    className="form-control"
-                    name="username"
-                    type="text"
-                    onChange={this.handleChange}
-                    value={this.state.form.username}
-                  />
-                </FormGroup>
-                 {/*
+              <input
+                className="form-control"
+                name="username"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.username}
+              />
+            </FormGroup> */}
+            {/*
                 <FormGroup>
                   <label>
                     Email: 
@@ -252,45 +260,45 @@ export class TablaUser extends React.Component {
                     value={this.state.form.type_user}
                   />
                 </FormGroup>  */}
-                <FormGroup>
-                  <label>
-                    Maxsize: 
+            <FormGroup>
+              <label>
+                Maxsize:
                   </label>
-                  <input
-                    className="form-control"
-                    name="maxsize"
-                    type="text"
-                    onChange={this.handleChange}
-                    value={this.state.form.maxsize}
-                  />
-                </FormGroup>
-              </ModalBody>
-    
-              <ModalFooter>
-                <Button
-                  color="primary"
-                  onClick={() => this.editar(this.state.form)}
-                >
-                  Editar
+              <input
+                className="form-control"
+                name="maxsize"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.maxsize}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Editar
                 </Button>
-                <Button
-                  color="danger"
-                  onClick={() => this.cerrarModalActualizar()}
-                >
-                  Cancelar
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Cancelar
                 </Button>
-              </ModalFooter>
-            </Modal>
-    
-    
-    
-            <Modal isOpen={this.state.modalInsertar}>
-              <ModalHeader>
-               <div><h3>Insertar Usuario</h3></div>
-              </ModalHeader>
-    
-              <ModalBody>
-                {/* <FormGroup>
+          </ModalFooter>
+        </Modal>
+
+
+
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+            <div><h3>Insertar Usuario</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            {/* <FormGroup>
                   <label>
                     Id: 
                   </label>
@@ -302,114 +310,115 @@ export class TablaUser extends React.Component {
                     value={this.state.data.length+1}
                   />
                 </FormGroup> */}
-                
-                <FormGroup>
-                  <label>
-                    Username: 
-                  </label>
-                  <input
-                    className="form-control"
-                    name="username"
-                    type="text"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-                
-                <FormGroup>
-                  <label>
-                    Email: 
-                  </label>
-                  <input
-                    className="form-control"
-                    name="email"
-                    type="text"
-                    onChange={this.handleChange}
-                  />
-                
-                </FormGroup>
 
-                <FormGroup>
-                  <label>
-                    Name: 
+            <FormGroup>
+              <label>
+                Username:
                   </label>
-                  <input
-                    className="form-control"
-                    name="name"
-                    type="text"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-               
-                <FormGroup>
-                  <label>
-                    Type_user: 
-                  </label>
-                  <input
-                    className="form-control"
-                    name="type_user"
-                    type="text"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
+              <input
+                className="form-control"
+                name="username"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
 
-                <FormGroup>
-                  <label>
-                    Maxsize: 
+            <FormGroup>
+              <label>
+                Email:
                   </label>
-                  <input
-                    className="form-control"
-                    name="maxsize"
-                    type="text"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-              </ModalBody>
-    
-              <ModalFooter>
-                <Button
-                  color="primary"
-                  onClick={() => this.insertar()}
-                >
-                  Insertar
+              <input
+                className="form-control"
+                name="email"
+                type="text"
+                onChange={this.handleChange}
+              />
+
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Name:
+                  </label>
+              <input
+                className="form-control"
+                name="name"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Type_user:
+                  </label>
+              <input
+                className="form-control"
+                name="type_user"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Maxsize:
+                  </label>
+              <input
+                className="form-control"
+                name="maxsize"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.insertar()}
+            >
+              Insertar
                 </Button>
-                <Button
-                  className="btn btn-danger"
-                  onClick={() => this.cerrarModalInsertar()}
-                >
-                  Cancelar
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
                 </Button>
-              </ModalFooter>
-            </Modal>
-          </>
-        );
-      }
-    }
-    
-   export class RegistroFila extends React.Component {
-
-
-      render() {
-         const dato = this.props.dato;
-          return (
-            <tr key={dato._id}>
-            <td>{dato._id}</td>
-            <td>{dato.username}</td>
-            <td>{dato.email}</td>
-            <td>{dato.name}</td>
-            <td>{dato.type_user}</td>
-            <td>{dato.maxsize}</td>
-            <td>
-              <Button
-                color="primary"
-                onClick={()=> this.props.mostrarModalActualizar(dato)}
-              >
-                Editar
-              </Button>{" "}
-              <Button color="danger" onClick={()=> this.props.eliminar(dato)}>Eliminar</Button>
-            </td>
-          </tr>
-          );
-      }
+          </ModalFooter>
+        </Modal>
+      </>
+    );
   }
+}
+
+export class RegistroFila extends React.Component {
+
+
+  render() {
+    const dato = this.props.dato;
+    console.log(dato);
+    return (
+      <tr key={dato._id}>
+        <td>{dato._id}</td>
+        <td>{dato.username}</td>
+        <td>{dato.email}</td>
+        <td>{dato.name}</td>
+        <td>{dato.type_user}</td>
+        <td>{dato.maxsize}</td>
+        <td>
+          <Button
+            color="primary"
+            onClick={() => this.props.mostrarModalActualizar(dato)}
+          >
+            Editar
+              </Button>{" "}
+          <Button color="danger" onClick={() => this.props.eliminar(dato)}>Eliminar</Button>
+        </td>
+      </tr>
+    );
+  }
+}
 
 export default TablaUser;
